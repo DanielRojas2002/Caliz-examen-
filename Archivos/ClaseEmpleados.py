b68090f534f16313dc937c630134200af1344a4e
Daniel Eduardo Rojas Garza <danymessi1000@gmail.com>
2020-05-26 17:58:33 -0500

    Inicialize la clase

---
+++ Archivos/ClaseEmpleados.py
@@ -0,0 +1,6 @@
+class Empleados ():
+    def __init__ (self,idEmpleado,nombre,direccion):
+        self.__idEmpleado=idEmpleado
+        self.__nombre=nombre
+        self.__direccion=direccion
+        
